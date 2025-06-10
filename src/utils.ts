10228189e2ff81771518a89533d1ae4b3af30a5e
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-06-10 19:18:50 +0700

    always get fresh file info

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
-import qs, { IParseBaseOptions } from "qs";
-import { Convert, File, IndexResponse, Sprite } from "./index-response";
+import qs from "qs";
+import { File, IndexResponse, Sprite } from "./index-response";
 import { Mime } from "mime";
 import { current_version, get_db, mapFile, stat, Storage } from "./db";
 
@@ -65,28 +65,10 @@ export function crop({ x, y, w, h }: Sprite): any {
   return { x, y, w, h };
 }
 
-export async function file_details(url: URL, env: Env, path: string, adapter: Storage): Promise<File | undefined> {
-  let file: File | undefined = undefined;
-  if (url.search) {
-    try {
-      file = Convert.toFile(qs.parse(url.search, qsConf));
-    } catch (e) {
-      console.warn("invalid parameters", JSON.stringify(qs.parse(url.search, qsConf)), e);
-    }
-  }
-  if (file) {
-    return file;
-  } else {
-    const found = await stat(path, adapter, env);
-    if (found) return mapFile(found, split_path(path)[0], await current_version(get_db(adapter, env)));
-  }
+export async function file_details(env: Env, path: string, adapter: Storage): Promise<File | undefined> {
+  const found = await stat(path, adapter, env);
+  if (found) return mapFile(found, split_path(path)[0], await current_version(get_db(adapter, env)));
 }
-
-const qsConf: IParseBaseOptions = {
-  ignoreQueryPrefix: true,
-  strictNullHandling: true,
-  decoder: (v, def, cs, t) => (t === "value" && /^\d+$/.test(v) ? +v : def(v, def, cs)),
-};
 
 export function processIndexResponse(response: IndexResponse, url: URL, env: Env): Response | PromiseLike<Response> {
   (response as any).dirname = url.searchParams.get("path") || "";
@@ -98,7 +80,7 @@ export function processIndexResponse(response: IndexResponse, url: URL, env: Env
 
   for (let f of response.files || []) {
     if (f.sprite) {
-			f.mime_type = "image/png";
+      f.mime_type = "image/png";
       (f as any).url = `${env.IMAGES}/${version}/${f.sprite.sheet}?format=png&${qs.stringify(crop(f.sprite))}`;
     } else if (f.basename.endsWith(".dds")) {
       f.mime_type = "image/png";
